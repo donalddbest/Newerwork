74525aa8d40785c5f5244aecb9c3c5a16a1413ee
donalddbest <donalddbest@gmail.com>
2018-07-24 18:58:47 -0400

    Alot has changed

--- Project/prettypictures.R
+++ Project/prettypictures.R
@@ -6,11 +6,11 @@
 rm(list = ls())
 library('dplyr')
 
-data = read.csv('profitfile.csv', header = FALSE)
+data = read.csv('costprofitfile.csv', header = FALSE)
 length(data[,1])
-greedy <- data %>% filter(V9 == 0) %>% mutate(TotalProfit = V1+V2+V3+V4+V5+V6+V7+V8)%>%arrange(TotalProfit)%>%mutate(weight = 1/length(TotalProfit))%>%mutate(probbefore = cumsum(weight))
+greedy <- data %>% filter(V10 == 80) %>% mutate(TotalProfit = V1+V2+V3+V4+V5+V6+V7+V8)%>%arrange(TotalProfit)%>%mutate(weight = 1/length(TotalProfit))%>%mutate(probbefore = cumsum(weight))
 arbitrary <- data %>% filter(V9 == 1) %>% mutate(TotalProfit = V1+V2+V3+V4+V5+V6+V7+V8)%>%arrange(TotalProfit)%>%mutate(weight = 1/length(TotalProfit))%>%mutate(probbefore = cumsum(weight))
-genes <- data %>% filter(V9 == 2) %>% mutate(TotalProfit = V1+V2+V3+V4+V5+V6+V7+V8)%>%arrange(TotalProfit)%>%mutate(weight = 1/length(TotalProfit))%>%mutate(probbefore = cumsum(weight))
+genes <- data %>% filter(V10 == 90) %>% mutate(TotalProfit = V1+V2+V3+V4+V5+V6+V7+V8)%>%arrange(TotalProfit)%>%mutate(weight = 1/length(TotalProfit))%>%mutate(probbefore = cumsum(weight))
 avegreedy <-c()
 avegenes<-c()
 for (j in 1:(length(data)-2)) {
@@ -28,15 +28,16 @@ dev.off()
 sum(avegreedy)
 # sum(avearb)
 sum(avegenes)
-png('newECDF.png')
-plot(ecdf(greedy$TotalProfit),verticals=TRUE, do.points=FALSE, col = 'blue',xlab = 'Total Profit',ylab = 'Empirical Cumulative Density', main = 'Comparing ECDFs of the Decision Rules')
-lines(ecdf(genes$TotalProfit),verticals=FALSE, do.points=FALSE, col = 'darkred',lwd = 3)
-legend(110000,1,c('Maximize Expected Profits','Maximize Expected Genes'),col = c('blue','darkred'),lty = c(1,2), lwd = c(1,3))
+png('costECDF.png')
+plot(ecdf(genes$TotalProfit),verticals=TRUE, do.points=FALSE, col = 'blue',xlab = 'Total Profit',ylab = 'Empirical Cumulative Density', main = 'Comparing ECDFs of the Decision Rules')
+lines(ecdf(greedy$TotalProfit),verticals=FALSE, do.points=FALSE, col = 'darkred',lwd = 3)
+legend(90000,1,c('Inseminate 6','Inseminate 5'),col = c('blue','darkred'),lty = c(1,2), lwd = c(1,3))
 dev.off()
 
 test<- ks.test(greedy$TotalProfit,genes$TotalProfit)
 test
-
+mean(greedy$TotalProfit)
+mean(genes$TotalProfit)
 # optimsixty <- optimum %>% filter(V7 == .6)
 # optimseventy <- optimum %>% filter(V7 == .7)
 
